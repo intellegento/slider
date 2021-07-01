2c7902f23e9f901a4a92542d6438c9333aa6bab0
intellegento <61728197+intellegento@users.noreply.github.com>
2021-07-01 17:07:10 +0600

    add left btn

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 let btnRight = document.querySelector(".btnRight");
+let btnLeft = document.querySelector(".btnLeft");
 let slides = document.querySelectorAll("img");
 let i = 0;
 
@@ -11,5 +12,17 @@ btnRight.addEventListener("click", function () {
   } else {
     slides[i - 1].classList.remove("block");
     slides[i].classList.add("block");
+  }
+});
+
+btnLeft.addEventListener("click", function () {
+  --i;
+  if (i >= slides.length) {
+    slides[i + 1].classList.add("block");
+    i = 0;
+    slides[i].classList.remove("block");
+  } else {
+    slides[i + 1].classList.add("block");
+    slides[i].classList.remove("block");
   }
 });
