c307f60c17b846e3cb959e041ec3f0c0f8150d35
intellegento <61728197+intellegento@users.noreply.github.com>
2021-07-03 08:32:38 +0600

    rewritten all code

--- script.js
+++ script.js
@@ -1,28 +1,22 @@
-let btnRight = document.querySelector(".btnRight");
-let btnLeft = document.querySelector(".btnLeft");
-let slides = document.querySelectorAll("img");
-let i = 0;
+const prev = document.querySelector(".prev");
+const next = document.querySelector(".next");
+const active = document.querySelector(".active");
+const slider = document.querySelector(".slider-items");
+const s = 991;
+let a = 0;
 
-btnRight.addEventListener("click", function () {
-  ++i;
-  if (i >= slides.length) {
-    slides[i - 1].classList.remove("block");
-    i = 0;
-    slides[i].classList.add("block");
-  } else {
-    slides[i - 1].classList.remove("block");
-    slides[i].classList.add("block");
-  }
+prev.addEventListener("click", (e) => {
+  console.log("prev");
 });
 
-btnLeft.addEventListener("click", function () {
-  --i;
-  if (i >= slides.length) {
-    slides[i + 1].classList.add("block");
-    i = 0;
-    slides[i].classList.remove("block");
-  } else {
-    slides[i + 1].classList.add("block");
-    slides[i].classList.remove("block");
-  }
+next.addEventListener("click", (e) => {
+  a = a - s;
+  slider.style.transform = "translate(" + a + "px, 0)";
+  console.log(a);
+});
+
+prev.addEventListener("click", (e) => {
+  a = a + s;
+  slider.style.transform = "translate(" + a + "px, 0)";
+  console.log(a);
 });
