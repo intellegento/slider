eea1dac22416bbbedd6a2369d1a7d98f731f5a41
intellegento <61728197+intellegento@users.noreply.github.com>
2021-07-04 21:44:10 +0600

    change property

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const prev = document.querySelector(".prev");
 const next = document.querySelector(".next");
 const active = document.querySelector(".active");
 const slider = document.querySelector(".slider-items");
-const s = 990;
+const s = 991;
 let a = 0;
 
 prev.addEventListener("click", (e) => {
