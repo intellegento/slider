b6e51ea4ddeec3ab4027ee740f77cea042fa875e
intellegento <61728197+intellegento@users.noreply.github.com>
2021-07-19 23:06:42 +0600

    ?

--- script.js
+++ script.js
@@ -5,7 +5,6 @@ const slider = document.querySelector(".slider-items");
 const s = 991;
 let a = 0;
 
-
 next.addEventListener("click", (e) => {
   a = a - s;
   slider.style.transform = "translate(" + a + "px, 0)";
@@ -15,4 +14,11 @@ next.addEventListener("click", (e) => {
   console.log(a);
 });
 
-
+prev.addEventListener("click", (e) => {
+  a = a + s;
+  slider.style.transform = "translate(" + a + "px, 0)";
+  if (a < 4495) {
+    a = -991;
+  }
+  console.log(a);
+});
